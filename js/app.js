a69d085d289a5c0f5391ba4c910b43df0c14a3d8
saikamara59 <mhsaidu@gmail.com>
2024-08-05 15:02:40 -0400

    started

---
+++ js/app.js
@@ -0,0 +1,43 @@
+/*-------------------------------------------Constants-----------------------------*/
+const characterNames = [
+    "Homelander",
+    "Soldier Boy",
+    "Stormfront",
+    "Frenchie",
+    "Black Noir",
+    "A-Train",
+    "Billy Butcher",
+    "Starlight",
+    "Queen Mae",
+    "Victoria Neuman",
+    "The Deep",
+    "Kimiko",
+    "Hughie",
+    "Ryan",
+    "Sage",
+    "Mothers Milk"
+]
+
+
+
+
+/*----------------------------Variables---------------------------*/
+
+let answer = ''
+let maxWrong = 8;
+let mistakes = 0;
+let guessed = [];
+
+
+/*----------------------------------------------Functions--------------------------------*/
+
+
+
+
+
+/* Cached Elements Refrences */
+
+
+
+
+/* Event Listeners */
