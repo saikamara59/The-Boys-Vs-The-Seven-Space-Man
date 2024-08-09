3392075647c4c5269a51b1d471548b7309cc585b
saikamara59 <mhsaidu@gmail.com>
2024-08-09 11:35:04 -0400

    updated

--- js/app.js
+++ js/app.js
@@ -37,62 +37,63 @@ const newGameBtnEl = document.querySelector('#new-game-button')
 const messageEl = document.querySelector('#message')
 const tileEl = document.querySelectorAll(".key-tile")
 let inputEl = document.querySelector("#input")
-
+const hangmanImageEl = document.querySelector('hangman-image')
 /*----------------------------------------------Functions--------------------------------*/
 
 function updateMessage() {
 messageEl.textContent= msg;
 }
 function updateDisplay() {
-    inputEl.value = guessedCharacter.join('')
+    inputEl.value = guessedCharacter.join('') // shows the guessed letters 
 }
 
 function checkGuess(){
-    let guess = inputEl.value 
-inputEl.value = ''
-let rightGuess = false
+    let guess = inputEl.value.toLowerCase(); // changes the guess to lowercase
+inputEl.value = ''; // Clears the input after getting the guess
+let rightGuess = false;
 for (let i = 0;i < selectedCharacter.length; i++){
-    if (selectedCharacter[i] === guess) {
-        guessedCharacter[i] = guess; 
+    if (selectedCharacter[i].toLowerCase() === guess) {
+        guessedCharacter[i] = selectedCharacter[i]; // update guessed character 
         rightGuess = true 
     
-    }
+    } // check if the guess matches any charachter in the selected character
     
 }
-
+// Handles incorect guesses
 if (!rightGuess) {
     if (!wrongTile.includes(guess)) {
         wrongTile.push(guess);
-        maxWrong--;
+        maxWrong--; // decreases the number of wrong attempts
     }
 }
 updateDisplay();
+updateHangmanImage()
 
 if (guessedCharacter.join('') === selectedCharacter){
-    msg = ("Congrats! you have guessed the character" + selectedCharacter + "!");
-    updateMessage()
+    msg = `Congrats! you have guessed the character ${selectedCharacter}!`;
+    updateMessage(msg); // shows win message
 } else if (maxWrong <= 0){
-msg = "Game Over"
-updateMessage()
+msg = `Game Over! the character was ${selectedCharacter}!`; 
+updateMessage(msg) // shows game over msg
 } else if(rightGuess) {
-    msg = "Great Job! keep going!"
-updateMessage()
+    msg = "Great Job! keep it going!"
+updateMessage(msg)
 } else {
-    msg = "Try Again Sorry!"
-    updateMessage()
+    msg = "Try Again!";
+    updateMessage(msg)
 }
 }
 function handleTileClick(e) {
-    console.log(e.target.textContent);
-    inputEl.value = e.target.textContent;
+    const guess = e.target.textContent.toLowerCase();
+    inputEl.value = guess;
     checkGuess();
 }
 
-function init(){
-    wrongTile = []
+function init() {
+    wrongTile = []; // clears the array of wrong guesses
     maxWrong = 6;
-    selectedCharacter = characterNames[Math.floor(Math.random()* characterNames.length)];
-    guessedCharacter = Array(selectedCharacter.length).fill('');
+    selectedCharacter = characterNames[Math.floor(Math.random() * characterNames.length)];
+    guessedCharacter = Array(selectedCharacter.length).fill('_');
     updateMessage("New game started! Make your first guess.");
     updateDisplay()
 }
@@ -105,21 +106,20 @@ function init(){
 
 tileEl.forEach((tile) => {
     tile.addEventListener("click",(e)=>{
-console.log(e.target.textContent);
-inputEl.value += e.target.textContent;
-console.log(inputEl)
+const guess = e.target.textContent.toLowerCase();
+inputEl.value = guess;
+checkGuess(guess);
 })
 })
-console.log(inputEl)
+// console.log(inputEl)
 
-function disableTiles(){
-    tileEl.forEach(tile => {
-        tile.removeEventListener("click",handleTileClick);
-    });
-}
+// function disableTiles(){
+//     tileEl.forEach(tile => {
+//         tile.removeEventListener("click",handleTileClick);
+//     });
+// }
 
 
 
-newGameBtnEl.addEventListener("click", init);
-
+newGameBtnEl.addEventListener("click",init);
 init();
